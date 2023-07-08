d3277931aae46f2b045aa5863715774e08c1a504
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-07-09 00:33:09 +0100

    new code update

--- zeal/js.js
+++ zeal/js.js
@@ -1,6 +1,9 @@
 const account_display = document.querySelector(".account_display");
 const followingButton = document.querySelector("#followingButton");
-
+const close_icon = document.querySelector("#close_icon");
+let Suggested_list = document.querySelector("#Suggested_list");
+const person_add_icon = document.querySelector("#person_add_icon");
+//////////////////////////////////////////////////////////////////
 function filer() {
   document.querySelector(".information-container").style.overflow = "hidden";
   account_display.style.display = "block";
@@ -9,6 +12,24 @@ function filer() {
   document.querySelector(".a").style.filter = "blur(2px)";
   document.querySelector(".sharing-of-photos").style.filter = "blur(2px)";
 }
+/////////////////////////////////////////////////////////////////
 followingButton.addEventListener("click", () => {
   filer();
+});
+function noneFiler() {
+  document.querySelector(".information-container").style.overflow = "none";
+  account_display.style.display = "none";
+  document.querySelector(".header").style.filter = "none";
+  document.querySelector("#charlesDetail").style.filter = "none";
+  document.querySelector(".a").style.filter = "none";
+  document.querySelector(".sharing-of-photos").style.filter = "none";
+}
+close_icon.addEventListener("click", () => {
+  noneFiler();
+});
+
+person_add_icon.addEventListener("click", () => {
+  if (Suggested_list.style.display === "none") {
+    Suggested_list.style.display = "block";
+  }
 });
